3195d60f86d93dbbdbeb619d352d1c59994cab9b
Josh Pollmann <joshpollmann102@gmail.com>
2021-02-22 09:50:39 -0500

    Update navbar styling and add additional components

--- board-game-site/src/app/app-routing.module.ts
+++ board-game-site/src/app/app-routing.module.ts
@@ -6,12 +6,16 @@ import { GameDetailsComponent } from './game-details/game-details.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { AboutComponent } from './about/about.component';
 import { DiscoverQueueComponent } from './discover-queue/discover-queue.component';
+import { HotListComponent } from './hot-list/hot-list.component';
+import { TopListComponent } from './top-list/top-list.component';
 
 const routes: Routes = [
   { path: 'game/:id', component: GameDetailsComponent },
   { path: 'search', component: SearchResultsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'discover', component: DiscoverQueueComponent },
+  { path: 'top-list', component: TopListComponent },
+  { path: 'hot-list', component: HotListComponent },
   { path: '', component: MainViewComponent },
   { path: '**', component: NotFoundComponent }
 ];
---
+++ board-game-site/src/app/hot-list/hot-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hot-list',
+  templateUrl: './hot-list.component.html',
+  styleUrls: ['./hot-list.component.scss']
+})
+export class HotListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
